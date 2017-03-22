a9206115d7bb070472a8643223800baaad3a51b2
dizhilik <dimzhilik2@gmail.com>
2017-03-22 15:50:22 +0200

    Исправление замечаний

--- app/js/plugin.js
+++ app/js/plugin.js
@@ -1,55 +1,48 @@
 ;(function ($) {
-	var moreOptionBtn = $('.more-option');
-	var secondForm = $('.second-form');
+    var moreOptionBtn = $('.more-option');
+    var secondForm = $('.second-form');
 
-	moreOptionBtn.on('click', function (){
-		secondForm.slideToggle();
-		moreOptionBtn.toggleClass('open');
-	});
-	//init sherch btn on header
-	$(function(){
-	var serchInputBtn = $('.serch-ic');
-	var serchInput = $('.serch-input');
+    moreOptionBtn.on('click', function () {
+        secondForm.slideToggle();
+        moreOptionBtn.toggleClass('open');
+    });
 
-	serchInputBtn.on('click', function (){
-		serchInput.slideToggle();
-		serchInputBtn.toggleClass('open');
-	});
+
 	//init slicknav 
 	$(function(){
 		$('.header-nav').slicknav({
-			label:'',
-			prependTo: '.header .container',
-		});
+            label: '',
+            prependTo: '.header .container',
+        });
+	});
 	//init fromstayler
-	 $('select').styler();
+		$('select').styler();
 	 //init slick slider
-	 $('.areas-slick-js').slick({
-		slidesToShow: 7,
-		slidesToScroll: 1,
-		autoplay: false,
-		autoplaySpeed: 2000,
-		responsive:[
-			{
-				 breakpoint: 1023,
-				 settings:{
-			        slidesToShow: 4,
-			    }
-			},
-			{
-				 breakpoint: 767,
-				 settings:{
-			        slidesToShow: 2,
-			    }
-			},
-			{
-				 breakpoint: 320,
-				 settings:{
-			        slidesToShow: 1,
-			    }
-			},
-		]
-	});
-	});
-});
+		 $('.areas-slick-js').slick({
+			slidesToShow: 7,
+			slidesToScroll: 1,
+			autoplay: false,
+			autoplaySpeed: 2000,
+			responsive:[
+				{
+					 breakpoint: 1023,
+					 settings:{
+						slidesToShow: 4,
+					}
+				},
+				{
+					 breakpoint: 767,
+					 settings:{
+						slidesToShow: 2,
+					}
+				},
+				{
+					 breakpoint: 481,
+					 settings:{
+						slidesToShow: 1,
+					}
+				},
+			]
+		 });
+
 })(jQuery);
